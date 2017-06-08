517f5b65cbeff2f9c104e5372f5b66022e7d0ee6
root <root@homestead.localdomain>
2017-06-09 01:29:18 +0800

    admin subdomain excluded'

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,7 +27,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::pattern('vue', '[\/\w\.-]*');
         Route::pattern('nonwww', '[\/\w\.-]*');
-        Route::pattern('username', '^(?!(?:static|my|support|api|superadmin|test|admin|www)$).*$');
+        Route::pattern('username', '^((?!admin).)*$');
         parent::boot();
     }
 
