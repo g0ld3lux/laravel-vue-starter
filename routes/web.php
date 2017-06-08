259d85e71acd0c921ed6ea25c1955583643b889b
root <root@homestead.localdomain>
2017-06-09 01:16:50 +0800

    test where params

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ Route::group(['domain' => 'www.'.config('app.domain')], function () {
     Route::get('/{vue?}', 'DomainController@vueRouterCatcher')->where('vue', '[\/\w\.-]*')->name('app');
 });
 
-Route::group(['domain' => '{username}.'.config('app.domain')], function () {
+Route::group(['domain' => '{username}.'.config('app.domain'), 'where' => ['username' => '^(?!(?:static|my|admin|www)$).*$']], function () {
     Route::get('/', function ($username) {
         return 'this is dashboard of ' .$username; 
     });
-})->where('username', '^(?!admin|support|api)\w*');
+});
 
 Route::group(['domain' => config('app.domain')], function () {
     Route::get('/{slug?}', 'DomainController@redirectNonWWWToWWW')->where('slug', '[\/\w\.-]*')->name('redirectToWWW');
