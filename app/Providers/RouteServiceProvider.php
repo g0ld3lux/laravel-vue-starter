cc4fa21b2f5dc29a401c42a0ad623d1080895743
root <root@homestead.localdomain>
2017-06-09 01:50:03 +0800

    router patter exclude list of words

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,7 +27,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::pattern('vue', '[\/\w\.-]*');
         Route::pattern('nonwww', '[\/\w\.-]*');
-        Route::pattern('username', '\b(admin|support|api|static)\b');
+        Route::pattern('username', '^(?!.*(admin|support|api|static|superadmin|helpdesk)).*$');
         parent::boot();
     }
 
